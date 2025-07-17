36f387ad0b05c5375db57ca0759b0db373c9f2fd
Ritina650 <ritinamaharjan62@gmail.com>
2025-07-17 16:44:24 +0545

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* typing animation */
+var type = new Typed(".typing",
+    {
+        strings:["Web Designer","Graphic Designer", "UIUX Designer"],
+        typeSpeed:100,
+        BackSpeed:60,
+        loop:true
+    }
+)
